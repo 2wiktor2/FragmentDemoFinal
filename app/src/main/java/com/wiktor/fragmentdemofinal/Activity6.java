57d891701571bee667cf3fd41837b43b036c9519
2wiktor2 <wiktor2011@gmail.com>
2020-07-17 17:39:35 +0300

    migrate to AndroidX

--- app/src/main/java/com/wiktor/fragmentdemofinal/Activity6.java
+++ app/src/main/java/com/wiktor/fragmentdemofinal/Activity6.java
@@ -1,7 +1,7 @@
 package com.wiktor.fragmentdemofinal;
 
         import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Activity6 extends AppCompatActivity {
 
